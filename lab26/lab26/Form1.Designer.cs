bd1bd87b7da87485061343d25251974a3876b71d
Larin-Pavlo-201TN-1 <98223565+Larin-Pavlo-201TN-1@users.noreply.github.com>
2023-05-21 16:56:46 +0300

    Add files via upload

---
+++ lab26/lab26/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace lab26
+{
+    partial class comment_txt
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Tel1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.Fax1 = new System.Windows.Forms.TextBox();
+            this.Name1 = new System.Windows.Forms.TextBox();
+            this.Name2 = new System.Windows.Forms.TextBox();
+            this.Fax2 = new System.Windows.Forms.TextBox();
+            this.Tel2 = new System.Windows.Forms.TextBox();
+            this.Theme = new System.Windows.Forms.TextBox();
+            this.date = new System.Windows.Forms.DateTimePicker();
+            this.comm = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(535, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Створити документ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ім\'я отримувача";
+            // 
+            // Tel1
+            // 
+            this.Tel1.Location = new System.Drawing.Point(187, 78);
+            this.Tel1.Name = "Tel1";
+            this.Tel1.Size = new System.Drawing.Size(187, 22);
+            this.Tel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Факс отримувача";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Телефон отримувача";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Тема";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(33, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Дата";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 166);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Коментарій";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(380, 84);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(152, 16);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Телефон відправника";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(380, 56);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(125, 16);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Факс відправника";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(380, 30);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(114, 16);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Ім\'я відправника";
+            // 
+            // Fax1
+            // 
+            this.Fax1.Location = new System.Drawing.Point(187, 52);
+            this.Fax1.Name = "Fax1";
+            this.Fax1.Size = new System.Drawing.Size(187, 22);
+            this.Fax1.TabIndex = 12;
+            // 
+            // Name1
+            // 
+            this.Name1.Location = new System.Drawing.Point(187, 27);
+            this.Name1.Name = "Name1";
+            this.Name1.Size = new System.Drawing.Size(187, 22);
+            this.Name1.TabIndex = 13;
+            // 
+            // Name2
+            // 
+            this.Name2.Location = new System.Drawing.Point(535, 27);
+            this.Name2.Name = "Name2";
+            this.Name2.Size = new System.Drawing.Size(187, 22);
+            this.Name2.TabIndex = 16;
+            // 
+            // Fax2
+            // 
+            this.Fax2.Location = new System.Drawing.Point(535, 52);
+            this.Fax2.Name = "Fax2";
+            this.Fax2.Size = new System.Drawing.Size(187, 22);
+            this.Fax2.TabIndex = 15;
+            // 
+            // Tel2
+            // 
+            this.Tel2.Location = new System.Drawing.Point(535, 78);
+            this.Tel2.Name = "Tel2";
+            this.Tel2.Size = new System.Drawing.Size(187, 22);
+            this.Tel2.TabIndex = 14;
+            // 
+            // Theme
+            // 
+            this.Theme.Location = new System.Drawing.Point(107, 107);
+            this.Theme.Name = "Theme";
+            this.Theme.Size = new System.Drawing.Size(187, 22);
+            this.Theme.TabIndex = 17;
+            // 
+            // date
+            // 
+            this.date.Location = new System.Drawing.Point(107, 135);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(187, 22);
+            this.date.TabIndex = 18;
+            // 
+            // comm
+            // 
+            this.comm.Location = new System.Drawing.Point(119, 166);
+            this.comm.Name = "comm";
+            this.comm.Size = new System.Drawing.Size(410, 70);
+            this.comm.TabIndex = 19;
+            this.comm.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(535, 191);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 26);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Переглянути шаблон";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comment_txt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 248);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comm);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.Theme);
+            this.Controls.Add(this.Name2);
+            this.Controls.Add(this.Fax2);
+            this.Controls.Add(this.Tel2);
+            this.Controls.Add(this.Name1);
+            this.Controls.Add(this.Fax1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Tel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "comment_txt";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Tel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox Fax1;
+        private System.Windows.Forms.TextBox Name1;
+        private System.Windows.Forms.TextBox Name2;
+        private System.Windows.Forms.TextBox Fax2;
+        private System.Windows.Forms.TextBox Tel2;
+        private System.Windows.Forms.TextBox Theme;
+        private System.Windows.Forms.DateTimePicker date;
+        private System.Windows.Forms.RichTextBox comm;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ lab26/lab26/Form1.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+using System.Diagnostics;
+
+namespace lab26
+{
+    public partial class comment_txt : Form
+    {
+        Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
+        Word.Document doc = new Word.Document();
+
+        public comment_txt()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                doc.Close();
+                app.Quit();
+                doc = null;
+                app = null;
+            }
+            catch { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] Findtxt = new string[] { "Name1", "Name2", "Fax1", "Fax2", "Tel1", "Tel2", "Theme", "date", "comm"};
+
+            object fileName = @"C:\Users\User\Desktop\Fax1.dotx";
+            object trueValue = true;
+            object missing = Type.Missing;
+
+            doc = app.Documents.Open(ref fileName, ref missing, ref trueValue,
+            ref missing, ref missing, ref missing, ref missing, ref missing,
+            ref missing, ref missing, ref missing, ref missing, ref missing,
+            ref missing, ref missing, ref missing);
+
+            app.Selection.Find.ClearFormatting();
+            app.Selection.Find.Replacement.ClearFormatting();
+
+            for(int i = 0; i < Findtxt.Length; i++)
+            {
+
+                object findText = Findtxt[i];
+                object replaceWith = Controls.Find(Findtxt[i], true)[0].Text;
+                object replace = 2;
+
+                app.Selection.Find.Execute(ref findText, ref missing, ref missing, ref missing,
+                ref missing, ref missing, ref missing, ref missing, ref missing, ref replaceWith,
+                ref replace, ref missing, ref missing, ref missing, ref missing);
+            }
+
+            app.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"C:\Users\User\Desktop\Fax1.dotx");
+        }
+    }
+}
